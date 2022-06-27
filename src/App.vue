bf8cdff4f33cc957d249215b90e2f69a63a5187c
Kang <kangxinpeng33@163.com>
2022-06-27 15:07:31 +0800

    add tab bar

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,43 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <tab-bar>
+    <tab-bar-item>
+      <template v-slot:icon>
+        <img src="~assets/img/tab-bar/home-icon@2x.png" alt="">
+      </template>
+      <template v-slot:text>
+        <div slot="text">首页</div>
+      </template>
+    </tab-bar-item>
+    <tab-bar-item>
+      <template v-slot:icon>
+        <img src="~assets/img/tab-bar/point-mall-icon@2x.png" alt="">
+      </template>
+      <template v-slot:text>
+        <div slot="text">积分商城</div>
+      </template>
+    </tab-bar-item>
+    <tab-bar-item>
+      <template v-slot:icon>
+        <img src="~assets/img/tab-bar/profile-icon@2x.png" alt="">
+      </template>
+      <template v-slot:text>
+        <div slot="text">我的</div>
+      </template>
+    </tab-bar-item>
+  </tab-bar>
 </template>
 
+<script>
+import TabBar from 'components/common/tab-bar/TabBar'
+import TabBarItem from 'components/common/tab-bar/TabBarItem'
+export default {
+  components: {
+    TabBar,
+    TabBarItem
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+  @import url(assets/css/base.css);
 </style>
---
+++ src/components/common/tab-bar/TabBarItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="tab-bar-item text-center">
+    <slot name="icon"></slot>
+    <div class="tab-bar-item-text">
+      <slot name="text" class="tab-bar-text"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  .tab-bar-item {
+    flex: 1;
+    font-size: 9px
+  }
+  .tab-bar-item img {
+    width: 22px;
+    height: 22px;
+  }
+  .tab-bar-item-text {
+    padding-top: 4px
+  }
+</style>
