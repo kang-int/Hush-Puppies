7f110a540514b8b7e8b242e78f2e70b3dad08bd3
kang <kangxinpeng33@163.com>
2022-08-20 21:33:32 +0800

    bug fix

--- src/views/profile/ProfileHeader.vue
+++ src/views/profile/ProfileHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="profile-header">
-    <img src="~assets/img/profile/dog-title@2x.png" alt="">
+    <img src="~assets/img/profile/dog-title@2x.png" alt="" />
     <div class="pos-center profile-photo">
       <slot name="photo"></slot>
     </div>
@@ -11,15 +11,19 @@
       <slot name="id"></slot>
     </div>
     <div class="pos-center profile-point">
-      <slot name="point"></slot>
+      <img src="~assets/img/profile/star-icon@2x.png" alt="" class="star-img" />
+      <div class="point"><slot name="point"></slot></div>
+      <img
+        src="~assets/img/profile/arrow-icon@2x.png"
+        alt=""
+        class="star-arrow"
+      />
     </div>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
 <style>
@@ -28,13 +32,14 @@ export default {
   left: 50%;
   transform: translateX(-50%);
 }
+
 .profile-header {
   position: relative;
   height: 338px;
-  background-color: #BF6050;
+  background-color: #bf6050;
 }
 
-.profile-header>img {
+.profile-header > img {
   position: absolute;
   left: 15px;
   top: 24px;
@@ -46,15 +51,18 @@ export default {
   width: 65px;
   height: 65px;
 }
+
 .profile-photo {
   top: 86px;
 }
+
 .profile-name {
   top: 155px;
   font-size: 17px;
   color: #fff;
   font-weight: bold;
 }
+
 .profile-id {
   top: 189px;
   font-size: 10px;
@@ -62,21 +70,31 @@ export default {
   color: #fff;
   opacity: 0.4;
 }
+
+.star-img {
+  width: 39px;
+  height: 39px;
+}
+
 .profile-point {
   top: 228px;
+  display: flex;
+  align-items: center;
   height: 50px;
-  border: 2px solid rgba(255,255,255,0.35);
+  border: 2px solid rgba(255, 255, 255, 0.35);
   border-radius: 26px;
-  background: url(assets/img/profile/star-icon@2x.png) no-repeat,
-              url(assets/img/profile/arrow-icon@2x.png) no-repeat;
-  background-size: 39px 39px, 6px 9px;
-  background-position: 0 5px, 119px center;
   font-size: 17px;
   font-weight: bold;
   color: #fff;
-  padding: 0 34px 0 48px;
 }
-.profile-point p {
-  margin-top: 13px;
+
+.profile-point .point {
+  margin: 0 18px 0 9px;
+}
+
+.star-arrow {
+  width: 6px;
+  height: 9px;
+  margin-right: 11px;
 }
 </style>
